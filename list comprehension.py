ea39a88084e29fb3d6a674e6c5a0e76a0bf1c5c8
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-08-05 22:50:20 +0530

    today

--- list comprehension.py
+++ list comprehension.py
@@ -1,4 +1,4 @@
-list comprehension of squares all even numbers between 1 and 20.
+# list comprehension of squares all even numbers between 1 and 20.
 
 req_list = [x**2 for x in range(1,21) if x%2 == 0]
 
